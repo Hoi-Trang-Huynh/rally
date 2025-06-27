3dda8350156e9cf8dd90b6f24bef7a64bcc5ab5e
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2025-06-28 06:02:10 +0700

    Learning Day-1: Expand upon Riverpod with shared_preferences

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,22 +1,49 @@
 // providers/theme_provider.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum AppThemeMode { light, dark, system }
 
-final themeProvider = StateNotifierProvider<ThemeNotifier, AppThemeMode>((ref) {
-  return ThemeNotifier();
+class ThemeState {
+  final AppThemeMode mode;
+  final bool isLoading;
+
+  ThemeState({required this.mode, required this.isLoading});
+}
+
+const _themePrefKey = 'app_theme_mode';
+
+final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeState>((ref) {
+  return ThemeNotifier(ref);
 });
 
-class ThemeNotifier extends StateNotifier<AppThemeMode> {
-  ThemeNotifier() : super(AppThemeMode.system);
+class ThemeNotifier extends StateNotifier<ThemeState> {
+  final Ref ref;
 
-  void setThemeMode(AppThemeMode mode) {
-    state = mode;
+  ThemeNotifier(this.ref)
+    : super(ThemeState(mode: AppThemeMode.system, isLoading: true)) {
+    _loadTheme(); // Load on init
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final index = prefs.getInt(_themePrefKey);
+    final mode =
+        (index != null && index >= 0 && index < AppThemeMode.values.length)
+            ? AppThemeMode.values[index]
+            : AppThemeMode.system;
+    state = ThemeState(mode: mode, isLoading: false);
+  }
+
+  Future<void> setThemeMode(AppThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themePrefKey, mode.index);
+    state = ThemeState(mode: mode, isLoading: false);
   }
 
   ThemeMode get MaterialThemeMode {
-    switch (state) {
+    switch (state.mode) {
       case AppThemeMode.light:
         return ThemeMode.light;
       case AppThemeMode.dark:
